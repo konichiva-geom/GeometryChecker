c36196e849dec074d7e5193a127c2a86b4f4611f
llesha <kononal@gmail.com>
2023-01-30 15:03:05 +0300

    added fuzztest of compression

--- src/test/kotlin/db/CompressionTest.kt
+++ src/test/kotlin/db/CompressionTest.kt
@@ -24,10 +24,27 @@ class CompressionTest {
         assertFails("Nothing to compress") { compress("") }
     }
 
+    @Test
+    fun fuzzTest() {
+        for (i in 0..100) {
+            val str = getRandomString()
+            testCompress(str)
+            println(str)
+        }
+    }
+
     private fun testCompress(text: String) {
         val map = compress(text)
         val (bytes, size) = encode(text, map)
         val res = decode(BitList.fromByteArray(bytes, size), map.entries.associateBy({ it.value }) { it.key })
         assertEquals(text, res)
+    }
+
+    private fun getRandomString(): String {
+        val res = StringBuilder()
+        for (i in 1..IntRange(2, 1000).random()) {
+            res.append(CharRange(Char.MIN_VALUE, Char.MAX_VALUE).random())
+        }
+        return res.toString()
     }
 }
